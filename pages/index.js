18656efd3067d236f340605a4a1d30ddbac5d839
ceejay077 <117851139+ceejay077@users.noreply.github.com>
2023-07-18 14:51:24 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -21,12 +21,12 @@ const Home = ({ frontmatter }) => {
 
 
       {/* Banner */}
-      <section className="section pb-[50px]">
-        <div className="container">
+      <section className="section pb-[50px] ">
+        <div className="container  ">
           <div className="row text-center">
             <div className="mx-auto lg:col-10">
-              <h1 className="font-primary font-bold">{banner.title}</h1>
-              <p className="mt-4">{markdownify(banner.content)}</p>
+              <h1 className="font-primary font-bold" style={{ textAlign: "center" }}>{banner.title}</h1>
+              <p className="mt-4" style={{ textAlign: "center" }}>{markdownify(banner.content)}</p>
               {banner.button.enable && (
                 <Link
                   className="btn btn-primary mt-4"
@@ -37,10 +37,10 @@ const Home = ({ frontmatter }) => {
                 </Link>
               )}
               <Image
-                className="mx-auto mt-12"
+                className=" mt-10"
                 src={banner.image}
-                width={750}
-                height={390}
+                width={1140}
+                height={1080}
                 alt="banner image"
                 priority
               />
